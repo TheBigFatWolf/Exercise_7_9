a56798fc3cd75ae847e305c6e18b4d9dae9a0005
Carlos <carloselcalvo@hotmail.com>
2020-11-19 22:39:51 +0100

    Cleaned code v.1.1

--- average.cs
+++ average.cs
@@ -1,16 +1,29 @@
 using System;
-public class Average{
-public static void Main(){
-double a=0, b, c=0, d;
-string s;
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();	// we read the mark as string to check the word "end"	 
-while(s!="end"){  // if the mark isn't an end then we pass it to double to work with it
-b=Convert.ToDouble(s);
-c++;   //counter of numbers
-a+=b;  // sum of all the notes
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();
+public class Average
+{
+	public static void Main()
+	{
+		double totalSum = 0, value = 0, numCounter = 0, marksAverage = 0;
+		string mark = "" ;
+		Console.Write("Insert a mark: ");
+		mark = Console.ReadLine(); 
+		while(mark != "end") // if the mark isn't an "end" then we pass it to double to work with it
+		{  
+			b = Convert.ToDouble(mark);
+			numCounter ++;
+			totalSum += b;  // We add the new mark value to the sum
+			Console.Write("Insert a mark: ");
+			mark = Console.ReadLine();
+		}
+		if(numCounter != 0)
+		{
+			marksAverage = totalSum / numCounter;
+			Console.WriteLine("The average is {0}.", marksAverage);
+		}
+		else{
+			Console.WriteLine("Invalid mark value.")
+		}
+	}
 }
 if(c!=0){
 	d=a/c;  //calculate the average
