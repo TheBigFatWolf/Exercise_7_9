ebca6fd20879b931a563896877d930f4a8b0643b
Carlos <carloselcalvo@hotmail.com>
2020-11-19 22:33:18 +0100

    Cleaned Code v.2.0

--- digits.cs
+++ digits.cs
@@ -1,35 +1,42 @@
 using System;
+public class Digits
+{
+    public static void Main()
+    {
+        int dOne = 0, dTwo = 0, dThree = 0, dMore = 0, input = 0;
+        string number = "";
 
-class digits
-{
- static void Main()
- {
-	 //counters of number of digits a is for one digit, b is for 2 digits, c is for 3 digits and d is for more than 3 digits
-     int a = 0, b = 0, c = 0, d = 0;
-      Console.Write("Insert a number: ");
-      // read the number as string to check the word "end"
-      string e = Console.ReadLine();
-     while(e!="end"){
-int input = Convert.ToInt32(e);
-       if(input != 0)
-{
-       if(input/10 == 0)
-           a ++;  // it only has one digit
-       else 
-       if (input/100 == 0)
-		b ++;  // the number has 2 digits
-        else
-        if (input/1000 == 0)
-        c ++;  // the number has 3 digits
-        else
-           d++;  // the number has more than 3 digits
+        Console.Write("Insert a number (tap end to finish): ");
+        number = Console.ReadLine();
+        while(number != "end")
+        {
+            input = Convert.ToInt32(number);
+            if (input != 0)
+            {
+                if (input/10 == 0)
+                {
+                    dOne ++;  // it only has one digit
+                }
+                else if (input/100 == 0)
+                {
+		            dTwo ++;  // the number has 2 digits
+                }
+                else if (input/1000 == 0)
+                {
+                    dThree ++;  // the number has 3 digits
+                }
+                else
+                {
+                    dMore ++;  // the number has more than 3 digits
+                }
+            }
+            else{
+                Console.WriteLine("'0' is not a valid enter.");
+            }
+            Console.Write("Insert another number (tap end to finish): ");
+            number = Console.ReadLine();
         }
-      e = Console.ReadLine();
+        Console.WriteLine("Quantity of numbers who has one, two, three or more digits:");
+        Console.WriteLine("One: {0}, Two: {1}, Three: {2}, More: {3}", dOne, dTwo, dThree, dMore);
+    }
 }
-
-Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", a, b, c, d); 
-
-
- }
-}
-
